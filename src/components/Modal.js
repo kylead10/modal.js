900153e064c14a6f95e4bcf1907b126d1e107f50
kylead10 <adiokayode111@gmail.com>
2023-03-14 15:07:30 +0100

    closing modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,86 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MdClose } from 'react-icons/md';
+
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalWrapper = styled.div`
+width : 800px;
+height : 500px
+box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+backbround: #fff;
+color: #000;
+display: grid;
+grid-template-colums: 1fr 1fr;
+postion: relative;
+z-index: 10;
+border-radius: 10px;
+`;
+const ModalImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px 0 0 10px;
+  background: #000;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: center;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
+
+  button {
+    padding: 10px 24px;
+    display: flex;
+    position: relative;
+    left: 30px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`;
+
+const CloseModalButton = styled(MdClose)`
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  height: 32px;
+  padding: 0;
+  z-index: 10;
+`;
 
 export const Modal = ({ showModal, setShowModal }) => {
-  return <>{showModal ? <div>Modal</div> : null}</>;
+  return (
+    <>
+      {showModal ? (
+        <Background>
+          <ModalWrapper showModal={showModal}>
+            <ModalImg src={require('./modal.jpg')} alt='camera picture' />
+            <ModalContent>
+              <h1>Are you ready ? </h1>
+              <button> Join Now!</button>
+            </ModalContent>
+            <CloseModalButton
+              aria-label='Close modal'
+              onClick={() => setShowModal((prev) => !prev)}
+            />
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
 };
